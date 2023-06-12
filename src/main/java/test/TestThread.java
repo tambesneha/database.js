efddfe258e51afe2c5728b3f34323e546ac1487a
ahoeffner <alex@hoeffner.net>
2023-06-12 11:01:43 +0200

    .

--- src/main/java/test/TestThread.java
+++ src/main/java/test/TestThread.java
@@ -23,7 +23,7 @@ package test;
 
 import java.net.URL;
 import javax.net.ssl.TrustManager;
-import database.js.security.FakeTrustManager;
+import database.rest.security.FakeTrustManager;
 
 
 public class TestThread extends Thread
